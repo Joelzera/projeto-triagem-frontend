e5ad97a00382410896f2a72507abacc1a0151a95
Joel Bispo <joelbispo04@hotmail.com>
2025-04-29 11:13:55 -0300

    criação da primeira tela

--- triagem-frontend/src/App.jsx
+++ triagem-frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { Avatar, Box, Typography } from '@mui/material';
+import imagem from '../img/logo.png'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
     <>
-    <h1>Iniciando a Triagem</h1>
+      <Box sx={{
+        backgroundColor: 'red',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        width: '100vw',
+        height: '100vh'
+      }}>
+        <Avatar src={imagem} sx={{ width: 100, height: 100, mb: 2}}></Avatar>
+        <Typography variant='h2' color='#fff'>Seja Bem vindo!</Typography>
+        <Typography variant='h4' color='#fff'>Toque na tela para continuar</Typography>
+      </Box>
     </>
   )
 }
